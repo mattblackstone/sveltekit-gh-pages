e881b013045e2ad327530d1828a43e4dbdcf362f
Matt Blackstone <matt@mattblackstone.com>
2023-04-27 23:19:37 -0700

    Updates

---
+++ _app/immutable/chunks/3.e2928249.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/about-page.svelte.6805d49e.js";export{t as component};
---
+++ _app/immutable/chunks/paths.bfa4f434.js
@@ -0,0 +1,1 @@
+var s;const l=((s=globalThis.__sveltekit_lsl7zz)==null?void 0:s.base)??"/sveltekit-gh-pages";var e;const t=((e=globalThis.__sveltekit_lsl7zz)==null?void 0:e.assets)??l;export{t as a,l as b};
